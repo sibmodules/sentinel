6081bb3b9f1471a6f078ebcab369806548f09748
Dmitry Sadovnikov <d.sadovnikov@admitad.com>
2018-02-05 13:06:35 +0300

    catch all exception from peewee to have no problems with govobjects

--- lib/models.py
+++ lib/models.py
@@ -85,7 +85,7 @@ class GovernanceObject(BaseModel):
 
             for item in golist.values():
                 (go, subobj) = self.import_gobject_from_dashd(dashd, item)
-        except (peewee.InternalError, peewee.OperationalError, peewee.ProgrammingError) as e:
+        except Exception as e:
             printdbg("Got an error upon import: %s" % e)
 
     @classmethod
@@ -140,7 +140,7 @@ class GovernanceObject(BaseModel):
         # get/create, then sync payment amounts, etc. from dashd - Dashd is the master
         try:
             subobj, created = subclass.get_or_create(object_hash=object_hash, defaults=subdikt)
-        except (peewee.OperationalError, peewee.IntegrityError) as e:
+        except Exception as e:
             # in this case, vote as delete, and log the vote in the DB
             printdbg("Got invalid object from dashd! %s" % e)
             if not govobj.voted_on(signal=VoteSignals.delete, outcome=VoteOutcomes.yes):
